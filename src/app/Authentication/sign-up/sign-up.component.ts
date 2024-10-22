c0a30959f557f361634671ea1afb45fdeeb257cf
ABHISHEK KR. GUPTA <abhishekkgupta20_ug@ece.nits.ac.in>
2024-10-22 11:29:40 +0530

    signup service integrated

--- src/app/Authentication/sign-up/sign-up.component.ts
+++ src/app/Authentication/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup,FormBuilder,FormControl, Validators } from '@angular/forms';
 import { HttpService } from './sharedData/http.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -12,7 +13,7 @@ export class SignUpComponent {
     mobile_no: string = '';
     password: string = '';
 
-    constructor (private http:HttpService,private formbuilder:FormBuilder){
+    constructor (private http:HttpService,private formbuilder:FormBuilder,private router:Router){
       this.signUpForm = formbuilder.group({
         name:['',Validators.required],
         mobile_no:['',Validators.required],
@@ -21,6 +22,24 @@ export class SignUpComponent {
     }
 
     onsignUpFormSubmit(){
-      
+      this.name=this.signUpForm.get('name')?.value;
+      this.mobile_no=this.signUpForm.get('mobile_no')?.value;
+      this.password=this.signUpForm.get('password')?.value;
+
+      const payload:any ={
+        name:this.name,
+        mobileNo:this.mobile_no,
+        password:this.password
+      }
+
+      this.http.SignUp("/registerUser",payload).subscribe({
+        next:(res:any)=>{
+          console.log("Signup successful : ",res);
+          this.router.navigate(['/']);
+        },
+        error:(err:any)=>{
+          console.log("Signup Failed : ",err);
+        }
+      })
     }
 }
